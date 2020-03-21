c25890a5e81d8bc8b1a21f0111f76d8806d50b65
Miraculous-V <dusworld@gmail.com>
2020-03-21 14:31:29 +0100

    Updates

---
+++ precache-manifest.9701dcf12b56f067c15bde42e3b31752.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe2a626d343a9e3dcbd7dff165b2aace",
+    "url": "/super-monsters/index.html"
+  },
+  {
+    "revision": "5a851d11aca34d179e3f",
+    "url": "/super-monsters/static/css/main.9bcb4a88.chunk.css"
+  },
+  {
+    "revision": "a7cb2eb2260630007bf2",
+    "url": "/super-monsters/static/js/2.4b0c48d1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/super-monsters/static/js/2.4b0c48d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a851d11aca34d179e3f",
+    "url": "/super-monsters/static/js/main.29a3d0f6.chunk.js"
+  },
+  {
+    "revision": "c588b3cfe89afa2c2695",
+    "url": "/super-monsters/static/js/runtime-main.b67826e7.js"
+  }
+]);
